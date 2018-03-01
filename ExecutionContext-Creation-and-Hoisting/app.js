7d1f56907cc9b76b6c6b10ec807ac39092ba4784
SL0TR <mohaiminislam94@gmail.com>
2018-03-01 14:38:49 +0600

    basic structure implemented and two js concept nodes added

---
+++ ExecutionContext-Creation-and-Hoisting/app.js
@@ -0,0 +1,10 @@
+// VARIABLES ARE STORED AS UNDEFINED IN THE MEMORY BY JS ENGINE, NOT THE ACTUAL CODE ITSELF
+var a = 'Hello World' 
+
+
+// FUNCTIONS ARE STORED AS IT IS ALSO IN THE MEMORY
+function b() {
+  console.log('Called b!')
+}
+b()
+console.log(a)
---
+++ TheGlobalEnvironment-and-TheGlobalObject/app.js
@@ -0,0 +1,7 @@
+// BASIC UNDERSTANDING OF THE WINDOW OBJECT OR THE GLOBAL OBJECT AND THIS VARIABLE
+
+var a = 'Hello World'
+
+function b() {
+  
+}
