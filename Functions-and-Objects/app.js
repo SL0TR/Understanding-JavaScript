6eec68261fd231f5dfbe3dfa3a8cb16cebb89f06
SL0TR <mohaiminislam94@gmail.com>
2018-03-18 15:31:23 +0600

    practice arrays, iife spread closures

--- Functions-and-Objects/app.js
+++ Functions-and-Objects/app.js
@@ -57,7 +57,7 @@ c = { greeting: 'howday'}
 console.log(d)
 console.log(c)
 
-// -----------------THIS--------------------//
+// -----------------THIS-------------------- //
 
 // This points to global window object
 console.log(this)
@@ -92,3 +92,152 @@ var chandu = {
 }
 
 chandu.log()
+
+// ---------------------ARRAYS--------------------- //
+
+// Can store any type of data on arrays because of JS's Dynamic Typing
+let arr = [
+    1,
+    false,
+    {
+        name: 'chandu',
+        address: 'chandur baari'
+    },
+    function(name) {
+        let greeting = 'Hello '
+        console.log(greeting + name)
+    },
+    'chika'
+]
+
+console.log(arr)
+arr[3](arr[2].name)
+
+
+
+// ---------------------ARGUMENTS-------------------- //
+
+function args(arg1, arg2, arg3) {
+    if (arguments.length === 0) {
+        console.log('Missing parameters yo')
+        return
+    }
+    console.log('------------------')
+    console.log(arg1)
+    console.log(arg2)
+    console.log(arg3)
+    console.log(arguments)
+    console.log('------------------')
+}
+
+args()
+
+// ---------------------SPREAD -------------------- //
+
+function args(arg1, arg2, arg3, ...other) {
+    console.log('------------------')
+    console.log(arg1)
+    console.log(arg2)
+    console.log(arg3)
+    console.log(other)
+    console.log(arguments)
+    console.log('------------------')
+}
+
+args('la', 'ka', 'chu', 'ni', 'chaas')
+
+
+// ---------------------IIFE-------------------- //
+
+var greeting = function(name) {
+    return 'I"m an ' + name;
+}('IIFE')
+
+console.log(greeting)
+
+var firstName = 'John';
+(function(name) {
+
+    var greeting = 'Inside IIFE: HELLO '
+    console.log(greeting + ' '+ name )
+
+}(firstName));
+
+
+// ---------------------CLOSURES-------------------- //
+
+function meet(whattosay) {
+    return function(name) {
+        console.log(whattosay + ' ' + name)
+    }
+}
+
+meet('Hi')('Toncy')
+
+// PROBLEM 1
+
+function buildFunction() {
+    var arr = []
+    
+    for(var i = 0; i < 3; i++) {
+        arr.push(
+            function() {
+                console.log(i)
+            }
+        )
+    }
+
+    return arr
+}
+
+var fs = buildFunction()
+
+fs[0]()
+fs[1]()
+fs[2]()
+
+// Solution to Problem 1 with ES5 IIFE
+
+function buildFunction2() {
+    var arr = []
+
+    for (var i = 0; i < 3; i++) {
+        arr.push( 
+            (function(j) {
+                return function() {
+                    console.log(j)
+                }
+            }(i))
+        )
+    }
+
+    return arr
+}
+
+var fs2 = buildFunction2()
+
+fs2[0]()
+fs2[1]()
+fs2[2]()
+
+// Solution to Problem 1 with ES6 Let
+
+function buildFunction3() {
+    var arr = []
+
+    for(let i = 0; i < 3; i++) {
+        arr.push(
+            function() {
+                console.log(i)
+            }
+        )
+    }
+
+    return arr
+}
+
+var fs3 = buildFunction3()
+
+fs3[0]()
+fs3[1]()
+fs3[2]()
