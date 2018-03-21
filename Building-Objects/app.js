3d6b7cc03da8f7307ce7321c07cbbbbb006e812d
SL0TR <mohaiminislam94@gmail.com>
2018-03-22 01:12:11 +0600

    practice object.create function constructor

--- Building-Objects/app.js
+++ Building-Objects/app.js
@@ -1,13 +1,37 @@
 // Function Constructor - 'new'
 
-function person(firstname, lastname) {
+function Person(firstname, lastname) {
     this.firstName = firstname
     this.lastName = lastname
 }
 
-var john = new person('John', 'Doe')
+Person.prototype.getFullName = function() {
+    return this.firstName + ' ' + this.lastName
+}
 
-var jane = new person('Jane', 'Johnson')
+var john = new Person('John', 'Doe')
 
-console.log(john)
+var jane = new Person('Jane', 'Johnson')
+
+console.log(john.getFullName())
 console.log(jane)
+
+
+// Function Constructor - Object.create
+
+
+
+var hooman = {
+    firstName: 'Default',
+    lastName: 'Default',
+    greet: function() {
+        return 'Hi ' + this.firstName
+    }
+}
+
+var carl = Object.create(hooman)
+
+carl.firstName = 'Carl'
+carl.lastName = 'Doe'
+
+console.log(carl.greet())
