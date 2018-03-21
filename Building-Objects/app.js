31b136670d998dcd77c4b0bde5c6df10076cafd4
SL0TR <mohaiminislam94@gmail.com>
2018-03-22 01:17:10 +0600

    add polyfill for older browser

--- Building-Objects/app.js
+++ Building-Objects/app.js
@@ -1,3 +1,20 @@
+// Polyfill
+
+if(!Object.create) {
+    Object.create = function(o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation' + ' only accepts the first parameter.')
+        }
+        function F() {
+            F.prototype = o
+            return new F()
+        }
+    }
+}
+
+
+
+
 // Function Constructor - 'new'
 
 function Person(firstname, lastname) {
