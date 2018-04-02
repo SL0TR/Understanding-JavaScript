da8f20ae097d94264200ce73f8d61955ba99cb09
SL0TR <mohaiminislam94@gmail.com>
2018-04-02 23:42:47 +0600

    practice this and arrow function

--- ES6-The-Right-Parts/index.js
+++ ES6-The-Right-Parts/index.js
@@ -1,5 +1,31 @@
 // Arrow Functions
 
-x = () => console.log('arrow functions!')
+x = () => console.log('arrow functions!');
 
-x()
+x();
+
+
+// Promise and This
+var obj = {
+  id: 42,
+  foo: function foo() {
+    setTimeout(function() {
+      console.log('Normal functions! ' + this.id)
+    }.bind(this),1000);
+  }
+
+};
+
+obj.foo(); // underfined without bind
+
+
+var obj2 = {
+  id: 42,
+  foo: function foo() {
+    setTimeout(() => {
+      console.log('arrow functions! ' + this.id)
+    },1000);
+  }
+};
+
+obj2.foo(); // underfined without bind
